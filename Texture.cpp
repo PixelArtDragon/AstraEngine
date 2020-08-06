ed9cc6724285c1824688d5eabd9716ced4509800
PixelArtDragon <lemezak@gmail.com>
2020-08-07 00:48:28 +0300

    Added mipmap generation when loading a texture

--- Texture.cpp
+++ Texture.cpp
@@ -19,6 +19,7 @@ Rendering::Texture::Texture(std::string image_file)
 	stbi_set_flip_vertically_on_load(true);
 	unsigned char* image = stbi_load(image_file.c_str(), &width, &height, &channels, STBI_rgb_alpha);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+	glGenerateMipmap(GL_TEXTURE_2D);
 	stbi_image_free(image);
 	//if (image.loadFromFile(image_file)) {
 		
